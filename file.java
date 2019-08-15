a74357289211d504e0dd2af4917224cc4b4420f3
bhumika chandrawat <cbhumika44@gmai.com>
2019-08-15 18:37:00 +0530

    my second commit

---
+++ file.java
@@ -0,0 +1,37 @@
+total 4381
+drwxr-xr-x 1 prata 197609       0 Jul 17 00:21 3D Objects/
+drwxr-xr-x 1 prata 197609       0 Jul  8 05:34 AppData/
+lrwxrwxrwx 1 prata 197609      30 Jul  8 05:34 Application Data -> /c/Users/prata/AppData/Roaming/
+drwxr-xr-x 1 prata 197609       0 Aug 14 03:01 bhumika-git-repo/
+drwxr-xr-x 1 prata 197609       0 Jul 17 00:21 Contacts/
+lrwxrwxrwx 1 prata 197609      58 Jul  8 05:34 Cookies -> /c/Users/prata/AppData/Local/Microsoft/Windows/INetCookies/
+drwxr-xr-x 1 prata 197609       0 Aug 11 18:24 Desktop/
+drwxr-xr-x 1 prata 197609       0 Jul 20 01:45 Documents/
+drwxr-xr-x 1 prata 197609       0 Aug 15 15:07 Downloads/
+drwxr-xr-x 1 prata 197609       0 Jul 17 00:21 Favorites/
+-rw-r--r-- 1 prata 197609       0 Aug 15 18:34 file.java
+-rw-r--r-- 1 prata 197609     858 Aug 15 16:32 file.txt
+drwxr-xr-x 1 prata 197609       0 Aug 14 07:50 IntelGraphicsProfiles/
+drwxr-xr-x 1 prata 197609       0 Jul 17 00:21 Links/
+lrwxrwxrwx 1 prata 197609      28 Jul  8 05:34 Local Settings -> /c/Users/prata/AppData/Local/
+drwxr-xr-x 1 prata 197609       0 Jul  8 05:39 MicrosoftEdgeBackups/
+drwxr-xr-x 1 prata 197609       0 Jul 17 00:21 Music/
+lrwxrwxrwx 1 prata 197609      24 Jul  8 05:34 My Documents -> /c/Users/prata/Documents/
+lrwxrwxrwx 1 prata 197609      66 Jul  8 05:34 NetHood -> /c/Users/prata/AppData/Roaming/Microsoft/Windows/Network Shortcuts/
+-rw-r--r-- 1 prata 197609 2097152 Aug 11 18:25 NTUSER.DAT
+-rw-r--r-- 1 prata 197609  626688 Jul  8 05:34 ntuser.dat.LOG1
+-rw-r--r-- 1 prata 197609  618496 Jul  8 05:34 ntuser.dat.LOG2
+-rw-r--r-- 1 prata 197609   65536 Jul  8 05:34 NTUSER.DAT{7e000bfd-4b4c-11e9-bd99-f8a2d6210b71}.TM.blf
+-rw-r--r-- 1 prata 197609  524288 Jul  8 05:34 NTUSER.DAT{7e000bfd-4b4c-11e9-bd99-f8a2d6210b71}.TMContainer00000000000000000001.regtrans-ms
+-rw-r--r-- 1 prata 197609  524288 Jul  8 05:34 NTUSER.DAT{7e000bfd-4b4c-11e9-bd99-f8a2d6210b71}.TMContainer00000000000000000002.regtrans-ms
+-rw-r--r-- 1 prata 197609      20 Jul  8 05:34 ntuser.ini
+drwxr-xr-x 1 prata 197609       0 Aug 14 07:51 OneDrive/
+drwxr-xr-x 1 prata 197609       0 Aug 15 16:22 Pictures/
+lrwxrwxrwx 1 prata 197609      66 Jul  8 05:34 PrintHood -> /c/Users/prata/AppData/Roaming/Microsoft/Windows/Printer Shortcuts/
+lrwxrwxrwx 1 prata 197609      55 Jul  8 05:34 Recent -> /c/Users/prata/AppData/Roaming/Microsoft/Windows/Recent/
+drwxr-xr-x 1 prata 197609       0 Jul 17 00:21 Saved Games/
+drwxr-xr-x 1 prata 197609       0 Jul 17 00:21 Searches/
+lrwxrwxrwx 1 prata 197609      55 Jul  8 05:34 SendTo -> /c/Users/prata/AppData/Roaming/Microsoft/Windows/SendTo/
+lrwxrwxrwx 1 prata 197609      59 Jul  8 05:34 Start Menu -> /c/Users/prata/AppData/Roaming/Microsoft/Windows/Start Menu/
+lrwxrwxrwx 1 prata 197609      58 Jul  8 05:34 Templates -> /c/Users/prata/AppData/Roaming/Microsoft/Windows/Templates/
+drwxr-xr-x 1 prata 197609       0 Aug 13 23:21 Videos/
